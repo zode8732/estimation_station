e0bb2a20881baa379e9d8b139b6314c0f2eb2e28
Vivaan Khambhati <vivaankhambhati@mac.lan>
2026-09-17 21:45:43 -0400

    more questions

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import json
 import random as rnd
 
-with open("data/questions.json", "r") as f:
-    data = json.load(f)
+def question_loop():
+    question__number = rnd.randint(0, len(questions) - 1)
+    expected_answer = questions[question__number]["answer"]
+    answer = int(input(questions[question__number]["question"] + " : "))
+    if answer == expected_answer:
+        print("correct answer")
+    else:
+        print("wrong answer")
 
 print("started game")
+with open("data/questions.json", "r") as f:
+    data = json.load(f)
 questions = data["questions"]
-question__number = rnd.randint(0, len(questions) - 1)
-expected_answer = questions[question__number]["answer"]
-answer = input(questions[question__number]["question"] + " : ")
+num_questions = int(input("how many questions? "))
+for _ in range(num_questions):
+    question_loop()
 
-if answer == expected_answer:
-    print("correct answer")
-else:
-    print("wrong answer")
+print("gg")
