22d70229472cf40c153ad9763a8a3d1949b14872
zode8732 <vivaan.khambhati@gmail.com>
2026-09-18 20:48:39 -0400

    functions

--- main.py
+++ main.py
@@ -2,15 +2,10 @@ import json
 import random as rnd
 import math as m
 
-global correct_answer
 correct_answer = 0
-global score
 score = 0
 
-def question_loop():
-    question__number = rnd.randint(0, len(questions) - 1)
-    expected_answer = questions[question__number]["answer"]
-    answer = int(input(questions[question__number]["question"] + " : "))
+def calculate_score(answer, expected_answer):
     global score
     if answer == expected_answer:
         print("correct answer")
@@ -20,9 +15,17 @@ def question_loop():
         score += 1000
     else:
         print("wrong answer")
-        temp_score = abs(score - expected_answer)
+        temp_score = abs(answer - expected_answer)
+        if temp_score == 0:
+            quit("score calculation error")
         temp_score = 10 *m.log10(temp_score)
         score += 1000 - temp_score
+
+def question_loop():
+    question__number = rnd.randint(0, len(questions) - 1)
+    expected_answer = questions[question__number]["answer"]
+    answer = int(input(questions[question__number]["question"] + " : "))
+    calculate_score(answer, expected_answer)
 
 print("started game")
 with open("data/questions.json", "r") as f:
