41ccbd36002955822d26106e2245fa82a24a7a6c
zode8732 <vivaan.khambhati@gmail.com>
2026-09-22 19:53:53 -0400

    result tracking

--- main.py
+++ main.py
@@ -4,15 +4,12 @@ import math as m
 
 correct_answer = 0
 score = 0
+results = []
 
 def calculate_score(answer, expected_answer):
-    global score
     if answer == expected_answer:
         print("correct answer")
-        global correct_answer
-        correct_answer += 1
-        
-        score += 1000
+        return 1000
     else:
         print("wrong answer")
         if answer == 0 or expected_answer == 0:
@@ -21,14 +18,21 @@ def calculate_score(answer, expected_answer):
             temp_score = 200 * abs(m.log2(abs(answer)) - m.log2(abs(expected_answer)))
             if temp_score > 1000:
                 temp_score = 999
-        score += 1000 - temp_score
         print(f"added score: {1000 - temp_score}")
+        return 1000 - temp_score
 
 def question_loop():
     question__number = rnd.randint(0, len(questions) - 1)
     expected_answer = questions[question__number]["answer"]
     answer = int(input(questions[question__number]["question"] + " : "))
-    calculate_score(answer, expected_answer)
+    global score
+    global results
+    points = calculate_score(answer, expected_answer)
+    score += points
+    results.append((answer, expected_answer, points))
+    global correct_answer
+    if answer == expected_answer:
+        correct_answer += 1
 
 print("started game")
 with open("data/questions.json", "r") as f:
@@ -41,3 +45,5 @@ for _ in range(num_questions):
 print("gg")
 print(f"score: {score}")
 print(f"correct answers: {correct_answer}/{num_questions}")
+
+# done
