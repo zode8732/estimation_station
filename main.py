2e4bc2fa8b687eef54f6be35575953c08effa595
zode8732 <vivaan.khambhati@gmail.com>
2026-09-23 23:08:45 -0400

    timer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 import random as rnd
 import math as m
+import time
+import threading
 
 correct_answer = 0
 score = 0
@@ -12,7 +14,7 @@ def calculate_score(answer, expected_answer):
         return 1000
     else:
         print("wrong answer")
-        if answer == 0 or expected_answer == 0:
+        if answer == 0 or expected_answer == 0 or answer == -1:
             temp_score = 999
         else:
             temp_score = 200 * abs(m.log2(abs(answer)) - m.log2(abs(expected_answer)))
@@ -25,7 +27,10 @@ def calculate_score(answer, expected_answer):
 def question_loop():
     question__number = rnd.randint(0, len(questions) - 1)
     expected_answer = questions[question__number]["answer"]
+    timer = threading.Timer(10.0, lambda: print(" time's up!\nusing -1\npress enter to continue"))
+    timer.start()
     answer = int(input(questions[question__number]["question"] + " : "))
+    timer.cancel()
     global score
     global results
     points = calculate_score(answer, expected_answer)
