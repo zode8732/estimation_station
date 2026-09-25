02cfa7c714dde30875cc69879c76d1f305efabab
zode8732 <vivaan.khambhati@gmail.com>
2026-09-24 21:04:00 -0400

    runs from main.py

--- game.py
+++ game.py
@@ -8,7 +8,7 @@ from questions import get_questions
 from scoring import calculate_score
 from statistics import print_stats
 
-def question_loop():
+def question_loop(questions):
     question__number = rnd.randint(0, len(questions) - 1)
     expected_answer = questions[question__number]["answer"]
     timer = threading.Timer(10.0, lambda: print(" time's up!\nusing -1\npress enter to continue"))
@@ -24,14 +24,17 @@ def question_loop():
     if answer == expected_answer:
         correct_answer += 1
 
-correct_answer = 0
-score = 0
-results = []
+def run():
+    global correct_answer
+    global score
+    global results
+    correct_answer = 0
+    score = 0
+    results = []
 
+    questions = get_questions()
+    num_questions = int(input("how many questions? "))
+    for _ in range(num_questions):
+        question_loop(questions)
 
-questions = get_questions()
-num_questions = int(input("how many questions? "))
-for _ in range(num_questions):
-    question_loop()
-
-print_stats(score, correct_answer, num_questions)
+    print_stats(score, correct_answer, num_questions)
--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 print("started game")
 
+game_state = True
+
+while game_state:
+    import game
+    game.run()
+    if input("play again? (y/n) : ") == "n":
+        game_state = False
+
 print("gg")
 
 # done
